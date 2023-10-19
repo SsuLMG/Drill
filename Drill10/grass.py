7e9f3160c7083401051ee761c9082b20c8321c7c
SsuLMG <seanseol@naver.com>
2023-10-19 12:45:36 +0900

    Drill 10 제출(2019180020 설찬형)

---
+++ Drill10/grass.py
@@ -0,0 +1,21 @@
+from pico2d import load_image
+
+class front_Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 70)
+
+    def update(self):
+        pass
+
+class back_Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 40)
+
+    def update(self):
+        pass
